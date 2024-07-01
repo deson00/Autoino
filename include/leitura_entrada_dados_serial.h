0d17ad1cc9aaacdbb832fcaaf88caca80d022f4c
Ederson Pereira de Brito <deson00@gmail.com>
2024-07-01 01:42:35 -0400

    definido o requer fuel conforme anterior

--- include/leitura_entrada_dados_serial.h
+++ include/leitura_entrada_dados_serial.h
@@ -160,7 +160,7 @@ void leitura_entrada_dados_serial()
           tipo_acionamento_injetor = values[9];
           tipo_combustivel = values[10];
           REQ_FUEL = values[11];
-          dreq_fuel = values[12]/numero_esguicho;
+          dreq_fuel = values[12];
           gravar_dados_eeprom_configuracao_injecao();
           tipo_vetor_configuracao_injecao = 0;
       }
